fce7b9e1457cac69edab64602f17ca2d645036b2
Leo Liang <leo.liang@ericsson.com>
2018-10-31 19:11:28 +0800

    Add more shift mod-taps

--- keyboards/handwired/dactyl_manuform/mine/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/mine/keymaps/default/keymap.c
@@ -12,6 +12,10 @@ extern keymap_config_t keymap_config;
 
 // Mod-Tap
 #define SFT_BSPC LSFT_T(KC_BSPC)
+#define SFT_A    LSFT_T(KC_A)
+#define SFT_Z    LSFT_T(KC_Z)
+#define SFT_SCLN LSFT_T(KC_SCLN)
+#define SFT_QUOT LSFT_T(KC_QUOT)
 #define CMD_DEL  LCMD_T(KC_DEL)
 #define CMD_SPC  RCMD_T(KC_SPC)
 #define ALT_CAPS ALT_T(KC_CAPS)
@@ -45,10 +49,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,----------------------------------,                             ,----------------------------------,
  * |   q  |   w  |   e  |   r  |   t  |                             |   y  |   u  |   i  |   o  |   p  |
  * |------+------+------+------+------|                             |-------------+------+------+------|
- * |   a  |   s  |   d  |   f  |   g  |                             |   h  |   j  |   k  |   l  |   ;  |
+ * |S^ a  |   s  |   d  |   f  |   g  |                             |   h  |   j  |   k  |   l  | ;  ^S|
  * |------+------+------+------+------|                             |------|------+------+------+------|
- * |      |      |      |      |      |                             |      |      |   (  |   )  |   "  |
- * |   z  |   x  |   c  |   v  |   b  |                             |   n  |   m  |   ,  |   .  |   '  |
+ * |      |      |      |      |      |                             |      |      |   (  |   )  | "    |
+ * |S^ z  |   x  |   c  |   v  |   b  |                             |   n  |   m  |   ,  |   .  | '  ^S|
  * '------+------+------+-------------'                             '-------------+------+------+------'
  *        |  {   |   }  |                                                         |  __  |   +  |
  *        |  [   |   ]  |                                                         |   -  |   =  |
@@ -65,13 +69,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_BASE] = LAYOUT( \
-  KC_Q, KC_W,    KC_E,  KC_R,    KC_T,                               KC_Y,    KC_U,  KC_I,    KC_O,   KC_P,    \
-  KC_A, KC_S,    KC_D,  KC_F,    KC_G,                               KC_H,    KC_J,  KC_K,    KC_L,   KC_SCLN, \
-  KC_Z, KC_X,    KC_C,  KC_V,    KC_B,                               KC_N,    KC_M,  KC_COMM, KC_DOT, KC_QUOT, \
-        KC_LBRC, KC_RBRC,                                                            KC_MINS, KC_EQL,          \
-                        SFT_BSPC,CMD_DEL,                            CMD_SPC, SFT_ENT,                         \
-                                     LY_NUM, CTL_TAB, ALT_CAPS, LY_NUM,                                        \
-                                     LY_FN,  KC_GESC, ____,     LY_FN                                          \
+  KC_Q,  KC_W,    KC_E,  KC_R,    KC_T,                               KC_Y,    KC_U,  KC_I,    KC_O,   KC_P,    \
+  SFT_A, KC_S,    KC_D,  KC_F,    KC_G,                               KC_H,    KC_J,  KC_K,    KC_L,   SFT_SCLN,\
+  SFT_Z, KC_X,    KC_C,  KC_V,    KC_B,                               KC_N,    KC_M,  KC_COMM, KC_DOT, SFT_QUOT,\
+         KC_LBRC, KC_RBRC,                                                            KC_MINS, KC_EQL,          \
+                         SFT_BSPC,CMD_DEL,                            CMD_SPC, SFT_ENT,                         \
+                                      LY_NUM, CTL_TAB, ALT_CAPS, LY_NUM,                                        \
+                                      LY_FN,  KC_GESC, ____,     LY_FN                                          \
 ),
 
 /* Layer: function + navigation
